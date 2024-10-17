2aa9780081a053c5d02eb7a8fce884f86b8945c9
Matthijs <matthijs.vanderlende@gmail.com>
2024-10-17 15:43:18 +0200

    added metricstracker to keep track of global metrics across runs. Seems to work fine

--- dmas_final_project/main.py
+++ dmas_final_project/main.py
@@ -7,10 +7,11 @@ import random
 from dmas_final_project.agents.official_news_agent import OfficialNewsAgent
 from dmas_final_project.agents.self_news_agent import SelfNewsAgent
 from dmas_final_project.agents.user_agent import UserAgent
+from dmas_final_project.data_processing.metrics_tracker import MetricsTracker
 from dmas_final_project.models.news_media_model import NewsMediaModel
 from dmas_final_project.parser.parser import parse_arguments, parse_news_media_model
 from dmas_final_project.plotting.plotting import plot_global_alignment_over_time, plot_evolution_by_dimension, \
-    plot_individual_alignment_over_time, plot_polarization, plot_social_network
+    plot_individual_alignment_over_time, plot_polarization, plot_social_network, plot_global_alignment_and_polarization
 from dmas_final_project.view.view import network_portrayal, get_server
 
 
@@ -25,22 +26,32 @@ def main() -> None:
         server.launch()
     elif params['mode'] == 'simulation':
         print("Running simulation...")
-        model = parse_news_media_model(params)
-        for _ in range(params['steps']):
-            model.step()
-        print("Simulation complete.")
-        plot_social_network(model)
-        plot_global_alignment_over_time(model.datacollector)
-        plot_evolution_by_dimension(model.datacollector, data_column='Opinion', label='Opinion')
-        plot_evolution_by_dimension(model.datacollector, data_column='Bias', label='Bias')
-        plot_individual_alignment_over_time(model.datacollector)
-        plot_polarization(model.datacollector)
+
+        num_runs = 10
+        metric_tracker = MetricsTracker()
+        for run in range(num_runs):
+            # Make sure to fix the seed so the social network is the same for each run!
+            model = parse_news_media_model(params)
+            model.set_metrics_tracker(metric_tracker)
+            for _ in range(params['steps']):
+                model.step()
+            print("Simulation complete.")
+
+            if run == num_runs:
+                plot_social_network(model)
+                plot_global_alignment_over_time(model.datacollector)
+                plot_evolution_by_dimension(model.datacollector, data_column='Opinion', label='Opinion')
+                plot_evolution_by_dimension(model.datacollector, data_column='Bias', label='Bias')
+                plot_individual_alignment_over_time(model.datacollector)
+                plot_polarization(model.datacollector)
+
+        plot_global_alignment_and_polarization(metric_tracker)
 
 
 if __name__ == "__main__":
     """
     python main.py interactive --config_file <config.json> --network_file <network_file>
     
-    python main.py simulation --config_file config.json --steps 200
+    python main.py simulation --config_file config.json --steps 1000
     """
     main()
