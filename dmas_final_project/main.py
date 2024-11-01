04fad2c68e4aa1c157c9eea868b8f59a6b06407b
Matthijs <matthijs.vanderlende@gmail.com>
2024-11-01 20:26:17 +0100

    changes?

--- dmas_final_project/main.py
+++ dmas_final_project/main.py
@@ -1,33 +1,23 @@
 import pickle
-
-import networkx as nx
 import numpy as np
-from matplotlib import pyplot as plt
-from mesa_viz_tornado.ModularVisualization import ModularServer
-from mesa_viz_tornado.modules import NetworkModule, ChartModule
 import random
-from dmas_final_project.agents.official_news_agent import OfficialNewsAgent
-from dmas_final_project.agents.self_news_agent import SelfNewsAgent
-from dmas_final_project.agents.user_agent import UserAgent
 from dmas_final_project.data_processing.data_aggregator import RunDataAggregator
 from dmas_final_project.data_processing.metrics_tracker import MetricsTracker
-from dmas_final_project.models.news_media_model import NewsMediaModel
 from dmas_final_project.parser.parser import parse_arguments, parse_news_media_model
-from dmas_final_project.plotting.plotting import plot_global_alignment_over_time, plot_evolution_by_dimension, \
-    plot_individual_alignment_over_time, plot_polarization, plot_social_network, plot_metrics, \
+from dmas_final_project.plotting.plotting import plot_evolution_by_dimension, \
+    plot_individual_alignment_over_time, plot_metrics, \
     plot_opinion_or_bias_frequency
-from dmas_final_project.view.view import network_portrayal, get_server
+from dmas_final_project.view.view import get_server
 import os
 
 
-def save_data(metric_tracker, aggr, path):
+def save_data(metric_tracker: MetricsTracker, aggr: RunDataAggregator, path: str) -> None:
     """
     Save the MetricsTracker and RunDataAggregator objects directly to files.
 
-    Parameters:
-    metric_tracker (MetricsTracker): The tracker recording metrics over runs.
-    aggr (RunDataAggregator): The data aggregator for collected run data.
-    path (str): The directory path to save the data files.
+    :param metric_tracker: The tracker recording metrics over runs.
+    :param aggr: The data aggregator for collected run data.
+    :param path: The directory path to save the data files.
     """
     # Save metric tracker
     with open(os.path.join(path, 'metric_tracker.pkl'), 'wb') as f:
@@ -56,13 +46,13 @@ def main() -> None:
     elif params['mode'] == 'simulation':
         print("Running simulation...")
 
+        # Fix seed to get reproducible results.
         random.seed(42)
         np.random.seed(42)
-        num_runs = 1
+        num_runs = 10        # This is a hardcoded value.
         metric_tracker = MetricsTracker()
         aggr = RunDataAggregator()
         for run in range(num_runs):
-            # Make sure to fix the seed so the social network is the same for each run!
             model = parse_news_media_model(params)
             model.set_metrics_tracker(metric_tracker)
             for _ in range(params['steps']):
@@ -71,13 +61,10 @@ def main() -> None:
 
             aggr.add_run_data(model.datacollector)
             if run == num_runs - 1:
-                plot_social_network(model)
-                plot_global_alignment_over_time(model.datacollector)
                 plot_evolution_by_dimension(model.datacollector, data_column='Opinion', label='Opinion')
                 if params.get('num_self_media') > 0:
                     plot_evolution_by_dimension(model.datacollector, data_column='Bias', label='Bias')
                 plot_individual_alignment_over_time(model.datacollector)
-                plot_polarization(model.datacollector)
 
         plot_opinion_or_bias_frequency(aggr, 'opinion', path=args.plot_path)
         if params.get('num_self_media') > 0:
@@ -93,6 +80,6 @@ if __name__ == "__main__":
     """
     python main.py interactive --config_file <config.json> --network_file <network_file>
 
-    python main.py simulation --config_file config.json --steps 2000
+    python main.py simulation --config_file config.json --steps 2000 --plot_path <path>
     """
     main()
